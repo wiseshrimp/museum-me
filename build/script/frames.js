83630c7a10794094275c29bbe4d4e28b06f582fe
Sue Roh <sr3022@columbia.edu>
2016-11-11 15:44:04 -0500

    Added walls and benches

---
+++ build/script/frames.js
@@ -0,0 +1,74 @@
+function loadImage (imageUrl, i, side){
+    var img = new Image();
+    img.src = imageUrl;
+    var entity = document.createElement("a-entity");
+
+    img.onload = function() {
+        var height = img.height / 170;
+        var width = img.width / 170;
+        
+        if (side === "front"){
+            var x = [-20, -10, 0, 10, 20];
+            var position = x[i] + " 4 -5.45";
+        }
+
+        else if (side === "left"){ // 19.45
+            var z = [0.45, 10.45, 20.45, 30.45, 40.45];
+            position = "-24.9 4 " + z[i];
+            entity.setAttribute("rotation", "0 90 0");
+        }
+
+        else if (side === "right"){
+            var z = [0.45, 10.45, 20.45, 30.45, 40.45];
+            position = "24.9 4 " + z[i];
+            entity.setAttribute("rotation", "0 -90 0");
+        }
+
+        else if (side === "back"){
+            var x = [-20, -10, 0, 10, 20];
+            position = x[i] + " 4 44.35";
+        }
+
+        entity.setAttribute("geometry", {primitive: "box"});
+        entity.setAttribute("geometry", "depth", 0.03);
+        entity.setAttribute("material", "src", "url(" + imageUrl + ")");
+
+        loadFrames(i, position, height, width, side);
+        // addLight(position, side);
+        entity.setAttribute("position", position);
+        entity.setAttribute("geometry", "height", height);
+        entity.setAttribute("geometry", "width", width);
+
+        scene.appendChild(entity);
+    }
+}
+
+function loadFrames(i, position, height, width, side){
+    coordinates = position.split(' ');
+    var frameTextures = ['frame-1.jpg', 'frame-2.jpg', 'frame-3.jpg', 'frame-4.jpg', 'frame-5.jpg', 'frame-6.png', 'frame-7.jpg'];
+    var j = Math.floor(Math.random() * 7);
+    var entity = document.createElement("a-entity");
+        entity.setAttribute("geometry", {primitive: "box"});
+        entity.setAttribute("material", "src", "url(" + "/textures/" + frameTextures[j] + ")");
+        entity.setAttribute("geometry", "height", height + 0.5);
+        entity.setAttribute("geometry", "width", width + 0.5);
+        entity.setAttribute("geometry", "depth", 0.1);
+
+    switch (side){
+        case "front":
+            entity.setAttribute("position", coordinates[0] + " 4 -5.5");
+            break;
+        case "left":
+            entity.setAttribute("position", "-24.95 4 " + coordinates[2]);
+            entity.setAttribute("rotation", "0 90 0");
+            break;
+        case "right":
+            entity.setAttribute("position", "24.95 4 " + coordinates[2]);
+            entity.setAttribute("rotation", "0 90 0");
+            break;
+        case "back":
+            entity.setAttribute("position", coordinates[0] + " 4 44.40");
+    }
+
+    scene.appendChild(entity);
+}
--- build/script/museum.js
+++ build/script/museum.js
@@ -9,22 +9,23 @@ var distance, coordinates;
 
 function loadMuseum (){
 
-    // Back wall photos
     var frontWallPhotos = photoBank.slice(0, 6);
     var leftWallPhotos = photoBank.slice(6, 12);
     var rightWallPhotos = photoBank.slice(12, 18);
     var backWallPhotos = photoBank.slice(18, 26);
+    // var statueHead = document.querySelector("#head");
+    // statueHead.setAttribute("material", "src", "url("+ photoBank[27] + ")");
+    // console.log(statueHead);
+    // // statueHead.setAttribute("position", "5 5 0")
+    // scene.appendChild(statueHead);
 
     // Front wall
     for (var i = 0; i < frontWallPhotos.length; i++){
-        distance = (i === 0) ? -18 : distance;
-        console.log(distance);
-        if (distance < 18) loadImage(frontWallPhotos[i], i, "front");
+        loadImage(frontWallPhotos[i], i, "front");
     }
 
     // Left wall
     for (var i = 0; i < leftWallPhotos.length; i++){
-        distance = (i === 0) ? -18 : distance;
         loadImage(leftWallPhotos[i], i, "left");
     }   
 
@@ -41,102 +42,9 @@ function loadMuseum (){
     buildWalls();
 }
 
-function loadImage (imageUrl, i, side){
-    var img = new Image();
-    img.src = imageUrl;
-
-    var entity = document.createElement("a-entity");
-        entity.setAttribute("geometry", {primitive: "box"});
-        entity.setAttribute("geometry", "depth", 0.03);
-        entity.setAttribute("material", "src", "url(" + imageUrl + ")");    
-
-    // else if (side === "left") {
-    //     entity.setAttribute("rotation", "0 90 0");
-    //     // var z = (i < 4) ? (i * 7) : (i * -7);
-    //     entity.setAttribute("position", "-20 4 " + i * -7);
-    // }
-
-    img.onload = function() {
-        var height = img.height / 170;
-        var width = img.width / 170;
-        
-        if (side === "front"){
-            var x = [-20, -10, 0, 10, 20];
-            var position = x[i] + " 4 -5.45";
-            loadFrames(i, position, height, width, side);
-        }
-
-        else if (side === "left"){ // 19.45
-            var z = [0.45, 10.45, 20.45, 30.45, 40.45];
-            position = "-24.9 4 " + z[i];
-            entity.setAttribute("rotation", "0 90 0");
-            loadFrames(i, position, height, width, side);
-        }
-
-        entity.setAttribute("position", position);
-        entity.setAttribute("geometry", "height", height);
-        entity.setAttribute("geometry", "width", width);
-
-        scene.appendChild(entity);
-    }
-}
-
-function loadFrames(i, position, height, width, side){
-    coordinates = position.split(' ');
-    var frameTextures = ['frame-1.jpg', 'frame-2.jpg', 'frame-3.jpg', 'frame-4.jpg', 'frame-5.jpg', 'frame-6.png', 'frame-7.jpg'];
-    var j = Math.floor(Math.random() * 7);
-    var entity = document.createElement("a-entity");
-        entity.setAttribute("geometry", {primitive: "box"});
-        entity.setAttribute("material", "src", "url(" + "/textures/" + frameTextures[j] + ")");
-        entity.setAttribute("geometry", "height", height + 0.5);
-        entity.setAttribute("geometry", "width", width + 0.5);
-        entity.setAttribute("geometry", "depth", 0.1);
-
-    switch (side){
-        case "front":
-            entity.setAttribute("position", coordinates[0] + " 4 -5.5");
-            break;
-        case "left":
-            entity.setAttribute("position", "-24.95 4 " + coordinates[2]);
-            entity.setAttribute("rotation", "0 90 0");
-            break;
-    }
-
-    scene.appendChild(entity);
-}
-
-function buildWalls(){
-    // BACK WALL:
-    var backWall = document.createElement("a-entity");
-        backWall.setAttribute("geometry", {primitive: "plane"});
-        backWall.setAttribute("geometry", "height", 30);
-        backWall.setAttribute("geometry", "width", 50);
-        backWall.setAttribute("position", "0 0 -5.55");
-
-    scene.appendChild(backWall);
-
-    // LEFT WALL:
-    var leftWall = document.createElement("a-entity");
-        leftWall.setAttribute("geometry", {primitive: "plane"});
-        leftWall.setAttribute("geometry", "height", 30);
-        leftWall.setAttribute("geometry", "width", 50);
-        leftWall.setAttribute("rotation", "0 90 0");
-        leftWall.setAttribute("position", "-25, 0, 19.45")
-    
-    scene.appendChild(leftWall);
-}
-
 /*
     TO DO: DO SOMETHING WITH STATUSES
 */
 function addWallText(statuses) {
     console.log('statuses: ', statuses);
-}
-
-function addLight(position) {
-    var light = document.createElement("a-light");
-        light.setAttribute("type", "point");
-        light.setAttribute("position", position);
-
-    scene.appendChild(light);
 }
